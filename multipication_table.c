72d45f85274b5be625a517b94105095e05d11d4f
ashashu24 <91936793+ashashu24@users.noreply.github.com>
2021-10-05 12:26:21 +0530

    Update multipication_table.c

--- multipication_table.c
+++ multipication_table.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
 
-int main()
-{
-    int num,i,lower,upper;
-    printf("enter the no:\n");
-    scanf("%d",&num);
-    printf("enter the limits\n");
-    scanf("%d\n%d",&lower,&upper);
-    for(i=lower;i<=upper;i++)
-    {
-        printf("%d*%d=%d\n",i,num,i*num);
-    }
-}
