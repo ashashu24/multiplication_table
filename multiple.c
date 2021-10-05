925e81b630b7216a2bffcf960ad6f4665596aee3
Ashitha Nair <67650148+ashithanair@users.noreply.github.com>
2021-10-05 12:53:47 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i,lower,upper;
+    printf("enter the no:\n");
+    scanf("%d",&num);
+    printf("enter the limits\n");
+    scanf("%d\n%d",&lower,&upper);
+    for(i=lower;i<=upper;i++)
+    {
+        printf("%d*%d=%d\n",i,num,i*num);
+    }
+}
